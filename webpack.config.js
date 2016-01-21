c7e6b38685ac02755a3bc9875a8f6e53c2a5b7a5
Charles Weems <charlie.weems@gmail.com>
2016-01-21 00:09:02 -0800

    Change bundle.js directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, ''),
     filename: 'bundle.js'
   },
   module: {
