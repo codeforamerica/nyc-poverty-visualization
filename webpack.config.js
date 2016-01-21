b090a07fcd288c6cbddaa9cf9260ebc513ffd1ff
Keith Kurson <keith@codeforamerica.org>
2016-01-21 14:43:19 -0800

    Add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
       test: /\.js$/,
       loaders: ['react-hot', 'babel?presets[]=es2015&presets[]=react'],
       include: path.join(__dirname, 'src')
+    },
+    {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
     }]
   }
 };
