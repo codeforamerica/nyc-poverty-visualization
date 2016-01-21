3a5c641640539caec7107c20857a2ab2b7b73389
Keith Kurson <keith@codeforamerica.org>
2016-01-21 15:27:41 -0800

    creating a cool scrolly thing

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+require('./styles/style.scss');
+
 ReactDOM.render(<App />, document.getElementById('root'));
