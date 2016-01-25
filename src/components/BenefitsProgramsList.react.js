1fed2405d36df7d0b7fb74b2f9b7275427aaeb77
Charles Weems <charlie.weems@gmail.com>
2016-01-25 14:25:25 -0800

    Fix error where componentdidmount didn't load fast enough and caused a crash at render

--- src/components/BenefitsProgramsList.react.js
+++ src/components/BenefitsProgramsList.react.js
@@ -4,26 +4,28 @@ import React, { Component } from 'react';
 // Bootstrap
 
 export default class BenefitsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      programs: []
+    };
+  }
   componentDidMount(){
     $.get('/api/v1/programs', function(result){
       this.setState({
         programs: result
       });
-      console.log(result);
     }.bind(this));
   }
 
-  constructor(props) {
-    super(props);
-  }
 
   render(){
     return(
       <div className='BenefitsList'>
         <ul>
-          {this.state.programs.map(function(program){
+          {this.state.programs.map(function(program, i){
             return(
-              <div>{program.program_name}</div>
+              <div key={i}>{program.program_name}</div>
             );
           }, this)}
         </ul>
