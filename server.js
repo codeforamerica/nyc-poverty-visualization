98e793ed2a433cd84431b67d6bbc39bd448a3627
Keith Kurson <keith@codeforamerica.org>
2016-01-28 15:49:50 -0800

    Moved stuff around, made server output nicer

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require('express');
 var path = require('path');
 var webpack = require('webpack');
 var app = express();
-
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, 'public');
+var port = process.env.PORT || 8080;
 
 app.use('/public', express.static(__dirname + '/public'))
   .get('/', function (req, res) {
     res.sendFile('index.html', {
       root: static_path
     });
-  }).listen(process.env.PORT || 8080, function (err) {
+  }).listen(port, function (err) {
     if (err) { console.log(err) };
-    console.log('Listening at localhost:8080');
+    console.log('Production server listening on http://localhost:'+port+' 💯');
   });
 
 if (isDevelopment) {
@@ -25,6 +25,6 @@ if (isDevelopment) {
     hot: true
   }).listen(3000, 'localhost', function (err, result) {
     if (err) { console.log(err) }
-    console.log('Listening at localhost:3000');
+    console.log('Development server listening from http://localhost:3000 🌏');
   });
 }
