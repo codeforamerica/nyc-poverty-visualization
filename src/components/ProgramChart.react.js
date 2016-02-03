587cdcf42f79e35a5935d9dd888657e7c85f3f84
Keith Kurson <keith@codeforamerica.org>
2016-02-03 09:41:34 -0800

    Smaller values

--- src/components/ProgramChart.react.js
+++ src/components/ProgramChart.react.js
@@ -12,12 +12,12 @@ export default class ProgramChart extends Component {
   constructor(props) {
     super(props);
     var defaultData = [
-      {text: 'HEAP', value: _.random(0.2, 6.2), class: ProgramColors('HEAP') },
-      {text: 'SNAP', value: _.random(0.2, 6.2), class: ProgramColors('SNAP') },
-      {text: 'Tax Refunds', value: _.random(0.2, 6.2), class: ProgramColors('TaxRefund')},
-      {text: 'WIC', value: _.random(0.2, 6.2), class: ProgramColors('WIC')},
-      {text: 'Childcare', value: _.random(0.2, 6.2), class: ProgramColors('ACSChildCare')},
-      {text: 'School Food', value: _.random(0.2, 6.2), class: ProgramColors('SchoolFood') },
+      {text: 'HEAP', value: _.random(0.2, 2), class: ProgramColors('HEAP') },
+      {text: 'SNAP', value: _.random(0.2, 2), class: ProgramColors('SNAP') },
+      {text: 'Tax Refunds', value: _.random(0.2, 2), class: ProgramColors('TaxRefund')},
+      {text: 'WIC', value: _.random(0.2, 2), class: ProgramColors('WIC')},
+      {text: 'Childcare', value: _.random(0.2, 2), class: ProgramColors('ACSChildCare')},
+      {text: 'School Food', value: _.random(0.2, 2), class: ProgramColors('SchoolFood') },
     ];
     this.state = { dataSet: defaultData, programCurrent: {}, width: 500 };
     this.handleBarClick = this.handleBarClick.bind(this);
@@ -67,7 +67,7 @@ export default class ProgramChart extends Component {
         <div>
           <BarChart
             width={this.state.width}
-            height={500}
+            height={400}
             margin={margin}
             data={data}
             onBarClick={this.handleBarClick }/>
