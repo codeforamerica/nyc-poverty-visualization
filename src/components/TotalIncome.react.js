03d5fd0c8d26fbd7563fda038636507244bd8ef2
Charles Weems <charlie.weems@gmail.com>
2016-02-03 14:13:32 -0800

    Add component for displaying CEO Poverty Threshold

--- src/components/TotalIncome.react.js
+++ src/components/TotalIncome.react.js
@@ -1,21 +1,16 @@
 "use strict";
 
 import React, { Component } from 'react';
+import formatDollars from '../controllers/formatDollars.js';
 
 export default class TotalIncome extends Component {
   constructor(props) {
     super(props);
   }
 
-  formatDollars(amount){
-    if(amount){
-      return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");      
-    }
-  }
-
   render() {
     return(
-      <h1 className="text-center">This family would make ${this.formatDollars(this.props.family.income)} a year and would be eligible to receive ${this.formatDollars(this.props.taxRefund)} in tax credits.</h1>
+      <h2 className="text-center">This family would make ${formatDollars(this.props.family.income)} a year and would be eligible to receive ${formatDollars(this.props.taxRefund)} in tax credits.</h2>
     );
   }
 }
