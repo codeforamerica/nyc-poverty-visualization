4088bbd878af6f44dc9d0947dc720a583d457c7d
Keith Kurson <keith@codeforamerica.org>
2016-02-03 09:38:50 -0800

    Footer & clicking on chart does things

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import Header from './components/Header.react.js';
+import Footer from './components/Footer.react.js';
 import Input from './components/Input.react.js'; // This will do more of the mangement
 import Story from './components/Story.react.js';
 import scrollTo from './helpers/Scroll.js';
@@ -28,6 +29,7 @@ export default class App extends Component {
           <Story id='pane1' shiftPane={this._shiftPane.bind(this)} ref="pane1" />
           <Input shiftPane={this._shiftPane.bind(this)} />
         </div>
+        <Footer />
       </div>
     );
   }
--- src/components/ProgramChart.react.js
+++ src/components/ProgramChart.react.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // import Chart, { Bar } from 'react-chartjs';
+import { Row, Col } from 'react-bootstrap';
 import BarChart from './BarChart/BarChart.react.js';
 import _ from 'lodash';
 import ProgramColors from '../controllers/ProgramColors.js';
@@ -10,7 +11,6 @@ import ProgramColors from '../controllers/ProgramColors.js';
 export default class ProgramChart extends Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
     var defaultData = [
       {text: 'HEAP', value: _.random(0.2, 6.2), class: ProgramColors('HEAP') },
       {text: 'SNAP', value: _.random(0.2, 6.2), class: ProgramColors('SNAP') },
@@ -19,27 +19,48 @@ export default class ProgramChart extends Component {
       {text: 'Childcare', value: _.random(0.2, 6.2), class: ProgramColors('ACSChildCare')},
       {text: 'School Food', value: _.random(0.2, 6.2), class: ProgramColors('SchoolFood') },
     ];
-    this.state = { dataSet: defaultData, width: 500 };
+    this.state = { dataSet: defaultData, programCurrent: {}, width: 500 };
+    this.handleBarClick = this.handleBarClick.bind(this);
+    this.programInfo = this.programInfo.bind(this);
     this.generateNewNumbers = this.generateNewNumbers.bind(this);
   }
   componentDidMount()  {
     var domNode = ReactDOM.findDOMNode(this);
-    console.log(domNode);
-
+    this.setState({width: domNode.offsetWidth}); // Need to set it at the start, too.
     window.onresize = () => {
      this.setState({width: domNode.offsetWidth});
     };
   }
   handleBarClick(element, id){
-    console.log(`The bin ${element.text} with id ${id} was clicked`);
+    var program = this.state.programCurrent;
+    program.name = element.text;
+    this.setState({ programCurrent: program });
   }
   generateNewNumbers() {
     var data = this.state.dataSet;
 
     //this.setState({ dataSet: data });
   }
+  programInfo() {
+    var programCurrent = this.state.programCurrent;
+
+    if (programCurrent.name) {
+      return(
+        <div className='programInfo'>
+          <Row>
+            <Col xs={8} sm={8} md={8} xsOffset={2} mdOffset={2} lgOffset={2}>
+              <div className='text-center'><h1>{programCurrent.name}</h1></div>
+              <div>Some information about the program currently selected will go here.</div>
+            </Col>
+          </Row>
+        </div>
+      );
+    }
+    return(<div></div>);
+  }
   render(){
     var data = this.state.dataSet;
+    var programCurrent = this.state.programCurrent;
     const margin = {top: 20, right: 20, bottom: 80, left: 40};
 
     return(
@@ -50,6 +71,7 @@ export default class ProgramChart extends Component {
             margin={margin}
             data={data}
             onBarClick={this.handleBarClick }/>
+            {this.programInfo()}
         </div>
     );
   }
