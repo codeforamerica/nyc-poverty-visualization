810ac3ffcca84a01974be7802fc03eb300d46ab9
Keith <keith@keithkurson.net>
2016-07-29 09:18:41 -0700

    Move away from react
    Made the choice to try and build this without react - there are only three pieces of moving data, and the functions that do the calculating don't rely on react at all.
    
    Put together a small service that uses gulp to browserify/babel/package all of the javascript (so that modules can still be used) into one file, and runs a development server with livereload so that functionality isn't lost.

---
+++ build/build.js
@@ -0,0 +1,35 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+var CEOPovertyTable = [[14425], [20345, 21778], [31156, 27417, 25859], [38104, 34708, 31156, 29692], [44553, 41375, 38104, 34708, 33306], [50629, 47638, 44553, 41375, 38104, 36764], [56392, 53526, 50629, 47638, 44553, 41375, 40098], [61907, 59165, 56392, 53526, 50629, 47638, 44553, 43307]];
+
+var povertyThreshold = function povertyThreshold(income, numberParents, numberChildren) {
+  var familySize = numberParents + numberChildren,
+      tableRow = CEOPovertyTable[familySize - 1];
+  if (numberChildren > 0) {
+    return tableRow[numberChildren];
+  } else {
+    return tableRow[0];
+  }
+};
+
+module.exports = povertyThreshold;
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+var _CEOPovertyThreshold = require('./calc/CEOPovertyThreshold.js');
+
+var _CEOPovertyThreshold2 = _interopRequireDefault(_CEOPovertyThreshold);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Have we changed the number of children or parents?
+$('#selectAdults').change(function () {
+  console.log("Changed number of parents");
+}); // Lets get the pieces
+
+},{"./calc/CEOPovertyThreshold.js":1}]},{},[2])
+
+
+//# sourceMappingURL=build.js.map
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Lets get the pieces
+import povertyThreshold from './calc/CEOPovertyThreshold.js';
+
+// Have we changed the number of children or parents?
+$('#selectAdults').change(function() {
+  console.log("Changed number of parents");
+});
