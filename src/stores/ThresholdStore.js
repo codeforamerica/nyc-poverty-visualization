94dc395905e2b30a30381077081bf5734e7950df
Keith <keith@keithkurson.net>
2016-08-08 10:46:38 -0700

    Bottom bar is sticky!

--- src/stores/ThresholdStore.js
+++ src/stores/ThresholdStore.js
@@ -3,7 +3,8 @@ import ThresholdActions from '../actions/ThresholdActions.js';
 
 class ThresholdStore {
   constructor() {
-    this.family = { adults: 2, children: 2, income: 15500 };
+    this.family = { adults: 2, children: 2, income: 15000 };
+    this.CEOPovertyThreshold = 31156;
 
     this.bindListeners({
       handleUpdateFamily: ThresholdActions.UPDATE_FAMILY
