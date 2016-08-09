f32e5db07062c418a17140dad862fdd48c140918
Keith <keith@keithkurson.net>
2016-08-09 09:52:49 -0700

    More stuff

--- src/components/BenefitsTable.react.js
+++ src/components/BenefitsTable.react.js
@@ -7,32 +7,26 @@ import { Table, Panel, Glyphicon } from 'react-bootstrap';
 import commaNumber from 'comma-number';
 export default class BenefitsTable extends Component {
   render() {
-    console.log(this.props.eligibility)
     return(
       <Table>
         <tbody>
           <tr>
-            <td><Glyphicon glyph="check" /></td>
             <td>SNAP</td>
             <td>${ commaNumber(this.props.eligibility.SNAP.snapAmount) }</td>
           </tr>
           <tr>
-            <td><Glyphicon glyph="check" /></td>
             <td>WIC</td>
             <td>${ commaNumber(this.props.eligibility.WIC.wicAmount) }</td>
           </tr>
           <tr>
-            <td></td>
             <td>Tax Credits</td>
             <td>${ commaNumber(this.props.eligibility.TaxRefund.refundAmount) }</td>
           </tr>
           <tr>
-            <td><Glyphicon glyph="check" /></td>
             <td>HEAP</td>
             <td>$ --</td>
           </tr>
           <tr>
-            <td><Glyphicon glyph="check" /></td>
             <td>School Lunches</td>
             <td>${ commaNumber(this.props.eligibility.SchoolFood.lunchValue) }</td>
           </tr>
